9fe9559f026a841416b8d7c9b6e8adc752c21fe7
yamini <vundelayamini@gmail.com>
2021-02-11 23:15:12 -0800

    Added : result of second computation

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -5,3 +5,7 @@ echo "Three inputs are :" $a $b $c
 
 result1=$(( $a + $b * $c ))
 echo " Result for the firt computation :"$result1
+
+result2=$(( $a * $b + $c ))
+echo "Result for the second computation :"$result2
+
