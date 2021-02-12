53febfc16a7e3a6ac3b5aa8a854a118901b5ccad
yamini <vundelayamini@gmail.com>
2021-02-12 01:18:51 -0800

    Added : read the values from Dict to Array

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash -x
 
 echo "Enter three inputs are :"a b c
@@ -5,6 +6,7 @@ read a b c
 echo "Three inputs are :" $a $b $c
 
 declare -A Allresult
+counter=1
 
 result1=$(($a + $b * $c))
 echo "Result for the first computation:" $result1
@@ -18,7 +20,14 @@ echo "Result for the third computation :"$result3
 result4=$(($a % $b + $c))
 echo "Result for the forth computation :"$result4
 
+echo "${!Allreult[@]}"
 echo "All result ${result[@]}"
 echo "${!result[@]}"
 
+counter[1]=${Allresult[result1]}
+counter[2]=${Allresult[result2]}
+counter[3]=${Allresult[result3]}
+counter[4]=${Allresult[result4]}
+
+echo  ${counter[@]}
 
