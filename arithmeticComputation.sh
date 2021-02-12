e155d71d9a291db50d6d049a3843e5374598b370
yamini <vundelayamini@gmail.com>
2021-02-11 23:22:31 -0800

    Added: result of third computation

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -9,3 +9,5 @@ echo " Result for the firt computation :"$result1
 result2=$(( $a * $b + $c ))
 echo "Result for the second computation :"$result2
 
+result3=$(( $c + $a / $b ))
+echo "Result for the third computation :"$result3
